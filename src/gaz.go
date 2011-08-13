e69d2723d950f76b51809a7ffaa44c272db98c9e
wingyplus <wingyminus@gmail.com>
2011-08-13 09:18:21 +0700

    edit content in extractField()

--- src/gaz.go
+++ src/gaz.go
@@ -13,7 +13,7 @@ type Database struct {
 }
 
 type DataSet struct {
-	db         *Database
+	db         Database
 	table_name string
 }
 
@@ -27,12 +27,12 @@ const (
 	pass     = "root"
 )
 
-func(conn *Connection) DB(db string) *Database {
-	return &Database{conn, db}
+func(conn *Connection) DB(db string) Database {
+	return Database{conn, db}
 }
 
-func(database *Database) C(table string) *DataSet {
-	return &DataSet{database, table}
+func(database Database) C(table string) DataSet {
+	return DataSet{database, table}
 }
 
 func(database *Database) new() {
@@ -60,9 +60,14 @@ func(database *Database) Query(query string) interface{} {
 }
 
 // extractField is return Field and Data_Types of Table
-func(dataset *DataSet) extractField() map[string]string {
-	
-	return map[string]string{"true":"true"}
+func(dataset *DataSet) extractField(name string) map[string]string {
+	dataset := new(Connection).DB("test").C("User")
+	rows := dataset.db.Query("DESC " + name).([]*mymy.Row)
+	field := make(map[string]string)
+	for _, row := range rows {
+		field[row.Str(0)] = row.Str(1)
+	}
+	return field
 }
 
 func(dataset *DataSet) Insert(p interface{}) (interface{}, bool) {
