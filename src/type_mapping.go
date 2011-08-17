6c79674e4273726bc7ea32cce1211e38c0511299
wingyplus <wingyminus@gmail.com>
2011-08-17 22:59:55 +0700

    add datatypes mapping

--- src/type_mapping.go
+++ src/type_mapping.go
@@ -1,6 +1,10 @@
 package gaz
 
 const mapping = map[string]string {
-	"string" : "VARCHAR",
-	"int" : "INT",
+	"string"   : "VARCHAR",
+	"byte"     : "TINYINT",
+	"int16"    : "SMALLINT",
+	"int"      : "INT",
+	"float32"  : "FLOAT",
+	"float64"  : "DOUBLE"
 }
